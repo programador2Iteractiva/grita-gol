164f5b67ea837225a1dfc2ea1e482cf536d89f75
Juan Camilo Mendoza Villegas <Juancamilomendozavillegas14@gmail.com>
2024-06-18 18:03:06 -0500

    up

--- js/changesViews.js
+++ js/changesViews.js
@@ -1,5 +1,9 @@
 const mic = document.getElementById('img-micro');
 const buttonPlay = document.getElementById('button-play');
+const componentStart = document.getElementById('gameStart');
 
+buttonPlay.addEventListener('click', () => {
+    mic.style.display = 'none';
+})
 
 
