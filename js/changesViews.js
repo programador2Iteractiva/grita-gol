c45cbac83c5fdcf0677807637b0436916e249602
Juan Camilo Mendoza Villegas <Juancamilomendozavillegas14@gmail.com>
2024-06-19 14:51:05 -0500

    up grito

--- js/changesViews.js
+++ js/changesViews.js
@@ -1,9 +1,14 @@
 const mic = document.getElementById('img-micro');
-const buttonPlay = document.getElementById('button-play');
 const componentStart = document.getElementById('gameStart');
+const buttonPlay = document.getElementById('button-play');
 
 buttonPlay.addEventListener('click', () => {
+
     mic.style.display = 'none';
+    buttonPlay.style.display = 'none';
+    componentStart.style.display = 'block';
+
+    localStorage.setItem('starting', true)
 })
 
 
